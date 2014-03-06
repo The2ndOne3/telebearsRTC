7cf71d2af9a10159d17e64b868ff58956f1a5f3d
Larry Xu <larry@larryxu.com>
2014-03-05 17:28:05 -0800

    Made sure angular works on account page

--- controllers/accounts.js
+++ controllers/accounts.js
@@ -52,7 +52,8 @@ module.exports = function(app) {
     }
     res.render('account', {
       user: req.user,
-      title: req.user.username
+      title: req.user.username,
+      angular: true
     });
   });
 
